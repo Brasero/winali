52123654dae51ddda09f9371f1e0093258e8d4aa
Brasero <brandonricci4@gmail.com>
2025-06-02 13:34:42 +0200

    updating next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    allowedDevOrigins: "true"
+    allowedDevOrigins: ["true"]
 };
 
 export default nextConfig;
