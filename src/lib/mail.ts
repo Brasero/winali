fc740bdee409e7dc6de9ad4a5467ed582570e8cb
Brasero <brandonricci4@gmail.com>
2025-06-02 23:02:31 +0200

    typing to and react argument in sendVerificationMail

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,13 +1,13 @@
 import {Resend} from "resend"
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-export const sendVerificationMail = async (to, react) => {
+export const sendVerificationMail = async (to: string, react: React.FC) => {
     try {
         const {data, error} = await resend.emails.send({
             from: "PartyChance <noreply@digicci.fr>",
             to: [to],
             subject: "Validation de votre inscription",
-            react: react
+            react
         })
 
         if (error) {
