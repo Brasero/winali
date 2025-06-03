17be80c74a396397391f4c96a5a804467a41fe05
Orima574 <boblion574@outlook.fr>
2025-06-03 20:34:06 +0200

    update

---
+++ src/app/authentification/layout.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/components/Navbar";
+
+export default function Layout({
+ children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navbar/>
+      <div className={"pt-18 hero-gradient p-5 h-screen m-0 flex flex-col justify-start items-center"}>
+        <h1 className={"items-center text-center font-bold text-[32px] my-5 "}>
+          <span className={" bg-linear-to-r from-brand-purple to-brand-coral bg-clip-text text-transparent"}>PartiChance</span>
+        </h1>
+        {children}
+      </div>
+    </>
+  )
+}
