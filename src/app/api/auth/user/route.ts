615c00f3b61a776a28ed973e10b25796e7cf69fe
Brasero <brandonricci4@gmail.com>
2025-06-05 00:57:15 +0200

    update

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -4,7 +4,7 @@ import {query} from "@/lib/db";
 export async function GET() {
     const session = await auth();
     console.log("session:", session);
-    if (!session || !session.user) {
+    if (!session || !session.user?.id) {
         return new Response(JSON.stringify({ error: "Unauthorized" }), {
             status: 401,
             headers: { "Content-Type": "application/json" },
