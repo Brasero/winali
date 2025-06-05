1e2034cef97546409bbb82203dda0ec65dc50ec9
Brasero <brandonricci4@gmail.com>
2025-06-05 14:05:03 +0200

    update

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+export const privateRoute: string[] = [
+    "/user/profil",
+    "/user/participations",
+    "/user/campaign",
+    "/api/auth/user"
+]
+
+export const authRoute: string[] = [
+    "/authentification/login",
+    "/authentification/signup",
+    "/authentification/forgot-password",
+    "/authentification/reset-password"
+]
