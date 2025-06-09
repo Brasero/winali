35340da366f5669ba3fdafb6520894b465e3778b
Brasero <brandonricci4@gmail.com>
2025-06-09 19:23:39 +0200

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
                     <Navbar />
                     {children}
                     <Footer />
-                </>) : ({children})
+                </>) : (children)
         }
     </SessionProvider>
     </body>
