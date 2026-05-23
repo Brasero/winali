d56067b67c9452f0b4fe2013bf2be2a64803a02f
Brasero <brandonricci4@gmail.com>
2026-05-23 16:25:52 +0200

    chore(stripe, version): updated stripe api version number

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-    apiVersion: "2025-05-28.basil",
+    apiVersion: "2025-08-27.basil",
     typescript: true,
     maxNetworkRetries: 3,
     telemetry: false,
